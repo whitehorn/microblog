d410d50153a61ae9a767b6484cbf591223f2c1e0
Pavel Shabanov <pa.shabanov@gmail.com>
2020-03-20 23:57:59 +0300

    new templates

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 
+
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }, 
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
